71888c6b566baf3e5f45b7d633519adda54d70f6
Artur <agavrik464@gmail.com>
2024-02-26 16:31:05 +0200

    js updated

--- js/script.js
+++ js/script.js
@@ -1,26 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const jsonData = [
-        {
-            "r030": 840,
-            "txt": "Долар США",
-            "rate": 38.3527,
-            "cc": "USD",
-            "exchangedate": "26.02.2024"
-        },
-        {
-            "r030": 978,
-            "txt": "Євро",
-            "rate": 41.5628,
-            "cc": "EUR",
-            "exchangedate": "26.02.2024"
-        }
-    ];
+function fetchCurrencyRates() {
+    fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
+        .then(response => response.json())
+        .then(data => {
+            updateCurrencyRates(data);
+        })
+        .catch(error => console.error('Ошибка получения данных о курсах валют:', error));
+}
 
+function updateCurrencyRates(data) {
     const currencyRatesDiv = document.getElementById('currency-rates');
+    currencyRatesDiv.innerHTML = ''; // Очищаем предыдущие данные
 
-    jsonData.forEach(currencyData => {
-        const currencyRateDiv = document.createElement('div');
-        currencyRateDiv.innerHTML = `<strong>${currencyData.txt}</strong>: ${currencyData.rate.toFixed(4)} ${currencyData.cc}`;
-        currencyRatesDiv.appendChild(currencyRateDiv);
+    const selectedCurrencies = ['USD', 'EUR'];
+
+    selectedCurrencies.forEach(currencyCode => {
+        const currencyData = data.find(item => item.cc === currencyCode);
+        if (currencyData) {
+            const currencyRateDiv = document.createElement('div');
+            currencyRateDiv.innerHTML = `<strong>${currencyData.txt}</strong>: ${currencyData.rate.toFixed(4)} ${currencyData.cc}`;
+            currencyRatesDiv.appendChild(currencyRateDiv);
+        }
     });
-});
+}
+
+// Получаем данные о курсах валют при загрузке страницы
+document.addEventListener('DOMContentLoaded', fetchCurrencyRates);
+
+// Обновляем данные каждый день (86400000 миллисекунд в сутках)
+setInterval(fetchCurrencyRates, 86400000);
