9571b107a251a7921de86aeb07a950a339307f11
Artur <agavrik464@gmail.com>
2024-03-11 17:00:10 +0200

    1234

--- js/script.js
+++ js/script.js
@@ -28,3 +28,31 @@ document.addEventListener('DOMContentLoaded', fetchCurrencyRates);
 
 // Обновляем данные каждый день (86400000 миллисекунд в сутках)
 setInterval(fetchCurrencyRates, 86400000);
+
+function startAnimation() {
+  const car = document.getElementById('car');
+  const road = document.getElementById('road');
+  const inputColumn = document.getElementById('inputColumn');
+
+  car.classList.add('clicked'); // Запускаем анимацию машины
+  
+  // После окончания анимации машины, скрываем дорогу и показываем колонку с инпутами
+  setTimeout(() => {
+    road.style.opacity = '0';
+    inputColumn.style.opacity = '1';
+    createInputs(); // Создаем инпуты
+  }, 1000); // Длительность анимации машины
+}
+
+// Функция для создания инпутов
+function createInputs() {
+  const inputColumn = document.getElementById('inputColumn');
+
+  for (let i = 0; i < 5; i++) { // Создаем 5 инпутов
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.placeholder = 'Введите данные';
+    inputColumn.appendChild(input);
+  }
+}
+
